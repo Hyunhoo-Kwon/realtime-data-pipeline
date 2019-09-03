c06b7b6282739e2d3efcd2fd6bd34ac3663dc14a
hyunhoo-kwon <kwongusgn@gmail.com>
2019-09-03 17:25:42 +0900

    init project

---
+++ kafka-spark-es-spring/src/main/java/com/study/kafkasparkesspring/KafkaSparkEsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.study.kafkasparkesspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSparkEsSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaSparkEsSpringApplication.class, args);
+    }
+
+}
