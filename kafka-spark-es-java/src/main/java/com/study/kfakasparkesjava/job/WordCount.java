9152510165efe274390b14180689a31c55d4cadf
hyunhoo-kwon <kwongusgn@gmail.com>
2019-09-03 14:11:31 +0900

    spark wordcount 집계 구현

--- kafka-spark-es-java/src/main/java/com/study/kfakasparkesjava/job/WordCount.java
+++ kafka-spark-es-java/src/main/java/com/study/kfakasparkesjava/job/WordCount.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.spark.SparkConf;
 import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaInputDStream;
 import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
@@ -20,33 +21,38 @@ import java.util.Map;
 public class WordCount {
 
     public void streamWordCount() {
-        SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount");
-        JavaStreamingContext streamingContext = new JavaStreamingContext(conf, Durations.seconds(10));
-
-        Map<String, Object> kafkaParams = new HashMap<>();
-        kafkaParams.put("bootstrap.servers", "localhost:9092");
-        kafkaParams.put("key.deserializer", StringDeserializer.class);
-        kafkaParams.put("value.deserializer", StringDeserializer.class);
-        kafkaParams.put("group.id", "use_a_separate_group_id_for_each_stream");
-        kafkaParams.put("auto.offset.reset", "latest");
-        kafkaParams.put("enable.auto.commit", false);
-
-        Collection<String> topics = Arrays.asList("test");
-
-        JavaInputDStream<ConsumerRecord<String, String>> stream =
-                KafkaUtils.createDirectStream(
-                        streamingContext,
-                        LocationStrategies.PreferConsistent(),
-                        ConsumerStrategies.<String, String>Subscribe(topics, kafkaParams)
-                );
-
-        JavaPairDStream<String, String> results = stream.mapToPair(record -> new Tuple2<>(record.key(), record.value()));
-        results.print();
-
         try {
+            SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount");
+            JavaStreamingContext streamingContext = new JavaStreamingContext(conf, Durations.seconds(10));
+
+            Map<String, Object> kafkaParams = new HashMap<>();
+            kafkaParams.put("bootstrap.servers", "localhost:9092");
+            kafkaParams.put("key.deserializer", StringDeserializer.class);
+            kafkaParams.put("value.deserializer", StringDeserializer.class);
+            kafkaParams.put("group.id", "use_a_separate_group_id_for_each_stream");
+            kafkaParams.put("auto.offset.reset", "latest");
+            kafkaParams.put("enable.auto.commit", true);
+
+            Collection<String> topics = Arrays.asList("test");
+
+            JavaInputDStream<ConsumerRecord<String, String>> stream =
+                    KafkaUtils.createDirectStream(
+                            streamingContext,
+                            LocationStrategies.PreferConsistent(),
+                            ConsumerStrategies.<String, String>Subscribe(topics, kafkaParams)
+                    );
+
+            JavaPairDStream<String, String> results = stream.mapToPair(record -> new Tuple2<>(record.key(), record.value()));
+            JavaDStream<String> lines = results.map(tuple2 -> tuple2._2());
+            JavaDStream<String> words = lines.flatMap(x -> Arrays.asList(x.split("\\s+")).iterator());
+            JavaPairDStream<String, Integer> wordCounts = words.mapToPair(s -> new Tuple2<>(s, 1)).reduceByKey((i1, i2) -> i1 + i2);
+
+            // results.print();
+            wordCounts.print();
+
             streamingContext.start();
             streamingContext.awaitTermination();
-        } catch (InterruptedException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
