454b21596719b22eccd36d6f31fd7270f4c57890
hyunhoo-kwon <kwongusgn@gmail.com>
2019-09-03 15:16:31 +0900

    spark streaming + elasticsearch 로컬 환경 구현

--- kafka-spark-es-java/src/main/java/com/study/kfakasparkesjava/job/WordCount.java
+++ kafka-spark-es-java/src/main/java/com/study/kfakasparkesjava/job/WordCount.java
@@ -11,6 +11,8 @@ import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.kafka010.ConsumerStrategies;
 import org.apache.spark.streaming.kafka010.KafkaUtils;
 import org.apache.spark.streaming.kafka010.LocationStrategies;
+import org.elasticsearch.spark.streaming.api.java.JavaEsSparkStreaming;
+import org.spark_project.guava.collect.ImmutableMap;
 import scala.Tuple2;
 
 import java.util.Arrays;
@@ -46,9 +48,16 @@ public class WordCount {
             JavaDStream<String> lines = results.map(tuple2 -> tuple2._2());
             JavaDStream<String> words = lines.flatMap(x -> Arrays.asList(x.split("\\s+")).iterator());
             JavaPairDStream<String, Integer> wordCounts = words.mapToPair(s -> new Tuple2<>(s, 1)).reduceByKey((i1, i2) -> i1 + i2);
+            JavaPairDStream<String, ImmutableMap<String, Integer>> wordCountsMap = wordCounts.mapToPair(tuple2 -> new Tuple2<>(tuple2._1, ImmutableMap.of("count", tuple2._2)));
 
             // results.print();
-            wordCounts.print();
+            wordCountsMap.print();
+
+            Map<String, String> esConf = new HashMap<>();
+            esConf.put("es.index.auto.create", "true");
+            esConf.put("es.nodes", "localhost");
+            esConf.put("es.port", "9200");
+            JavaEsSparkStreaming.saveToEsWithMeta(wordCountsMap, "spark", esConf);
 
             streamingContext.start();
             streamingContext.awaitTermination();
