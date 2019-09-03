e2ff68ce77ecfa3f8122cc6b49d04186ef7f3a52
hyunhoo-kwon <kwongusgn@gmail.com>
2019-09-03 17:41:29 +0900

    kafka + spark + es with spring 구현

--- kafka-spark-es-spring/src/main/java/com/study/kafkasparkesspring/KafkaSparkEsSpringApplication.java
+++ kafka-spark-es-spring/src/main/java/com/study/kafkasparkesspring/KafkaSparkEsSpringApplication.java
@@ -1,5 +1,6 @@
 package com.study.kafkasparkesspring;
 
+import com.study.kafkasparkesspring.job.WordCount;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,8 @@ public class KafkaSparkEsSpringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KafkaSparkEsSpringApplication.class, args);
+        WordCount wordCount = new WordCount();
+        wordCount.streamWordCount();
     }
 
 }
